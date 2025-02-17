a792ada5a72d4730cfbb518e29bd026f6653b845
Om Mistry <ommistry5559@gmail.com>
2025-02-17 16:11:51 +0530

    Final commit

---
+++ code/P/L.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(j==1 || i==n)
+            {
+                cout << "* ";
+            }
+            else{
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
