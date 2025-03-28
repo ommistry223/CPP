3eed911a0a29de747712047d4685ed8c869da3a8
Om Mistry <ommistry5559@gmail.com>
2025-03-28 19:23:18 +0530

    Add A B & Z patterns

---
+++ code/C Program/Patterns/B.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == 1 || j == 1 || i == n || j == n || i == n / 2 + 1) {
+                if ((i == 1 && j == n) || (i == n && j == n) || (i == n / 2 + 1 && j == n)) {
+                    printf("  ");
+                } else {
+                    printf("* ");
+                }
+            } else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
