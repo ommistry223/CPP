85197faae89f4980cb653e37c9bcf5d4e6784692
Om Mistry <ommistry5559@gmail.com>
2025-03-31 18:41:19 +0530

    Add a new patterns in c program --H,I,L

---
+++ code/Prectice/const.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    static int i=3;
+    for(i=0; i<4; i++)
+    {
+        cout<< i<< endl;
+        int n;
+        
+
+    }
+    return 0;
+    
+}
