22e2d78135711d89e3a1f02a45c4491783613f13
Om Mistry <ommistry5559@gmail.com>
2025-04-14 15:34:24 +0530

    Final commit

---
+++ code/Prectice/A4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the row: ";
+    cin >> n;
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(i==1 || j==1 || j==n || i==n)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
